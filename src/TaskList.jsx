b7c76ce1eea8b89b77a87af42f3d55aae6bb2767
Gabriella Melo <gabriella.melo0119@gmail.com>
2025-02-03 01:51:28 -0300

    Add TaskList initial structure

---
+++ src/TaskList.jsx
@@ -0,0 +1,21 @@
+import TaskCard from "./TaskCard"
+
+const TaskList = () => {
+    return (
+        <div className="task-list-wrapper">
+
+            <div className="task-list-wrapper__header-wrapper">
+
+                <h1 className="task-list-wrapper__header-wrapper__title">Notes</h1>
+
+                <button className="task-list-wrapper__header-wrapper__title__erase-all"></button>
+
+            </div>
+
+            <TaskCard />
+
+        </div>
+    )
+}
+
+export default TaskList
